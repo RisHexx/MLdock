9038b2657dd135bab5510440a216dc58643b5342
Rishexx <privaterishabhh@gmail.com>
2026-07-14 02:46:37 +0530

    added comments to Schema

--- backend/app/schemas/api_key.py
+++ backend/app/schemas/api_key.py
@@ -1,13 +1,20 @@
 from pydantic import BaseModel, ConfigDict, Field
+#Every Pydantic model inherits from BaseModel.
+#Used to configure how the model behaves.
+#Adds validation rules.
+
 from datetime import datetime
 
 
 class APIKeyCreate(BaseModel):
     name: str = Field(..., min_length=1, max_length=100)
+    #... means this field is required
 
 
 class APIKeyResponse(BaseModel):
+    #Every Pydantic model has some settings that control its behavior. that presnt in model_config
     model_config = ConfigDict(from_attributes=True)
+    #from_attributes=True → Pydantic can also accept objects not only dict and reads their attributes
 
     id: str
     name: str
